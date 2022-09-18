0ad1638031fab182ed9ebdfeea933bba20d36881
ekgus <ekgus@DESKTOP-EA7PAE3>
2022-09-18 20:51:08 +0900

    초기 커밋.

---
+++ ConsoleApp1/WinFormApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+class WinFormApp : Form
+{
+    WinFormApp()
+    {
+        Text = "Hello World!";
+    }
+    public static void main()
+    {
+        Application.Run(new WinFormApp());
+    }
+}
